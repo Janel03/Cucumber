18f5d927fe51acd69ebfbf6f298189bc86f491f3
Janel03 <s.zhanel@gmail.com>
2022-12-11 19:17:35 -0500

    Update

--- src/test/java/utils/CommonMethods.java
+++ src/test/java/utils/CommonMethods.java
@@ -34,7 +34,6 @@ public class CommonMethods extends PageInitializer {
         driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT, TimeUnit.SECONDS);
     initializePageObjects();
     }
-
     public static void closeBrowser(){
         driver.quit();
     }
@@ -44,37 +43,30 @@ public class CommonMethods extends PageInitializer {
         element.clear();
         element.sendKeys(textToSend);
     }
-
     //to get webdriver wait
     public static WebDriverWait getWait(){
         WebDriverWait wait = new WebDriverWait(driver, Constants.EXPLICIT_WAIT);
         return wait;
     }
-
     //this method will wait for the element to be clickable
     public static void waitForClickability(WebElement element){
         getWait().until(ExpectedConditions.elementToBeClickable(element));
     }
-
     //this method will perform click operation but before perform click, it will wait
     //for the element to be clickable
     public static void click(WebElement element){
         waitForClickability(element);
         element.click();
     }
-
     //this method will return JavascriptExecutor Object
-
     public static JavascriptExecutor getJSExecutor(){
         JavascriptExecutor js = (JavascriptExecutor) driver;
         return js;
     }
-
     //this function will perform click on element using javascript executor
     public static void jsClick(WebElement element){
         getJSExecutor().executeScript("arguments[0].click();", element);
     }
-
     //selecting the dropdown using text
     public static void selectDropdown(WebElement element, String text){
         Select s = new Select(element);
